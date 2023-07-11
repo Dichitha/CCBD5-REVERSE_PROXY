27c17122e390a3038cc7021f3bd60986e0bf42de
Varun Mk <mkvarun2001@gmail.com>
2023-07-11 19:38:23 +0530

    Changed file structure

---
+++ webserver/routes/index.js
@@ -0,0 +1,27 @@
+const url = require('url')
+const express = require('express')
+const router = express.Router()
+const needle = require('needle')
+
+const API_BASE_URL = process.env.API_BASE_URL
+const API_KEY_NAME = process.env.API_KEY_NAME
+const API_KEY_VALUE = process.env.API_KEY_VALUE
+
+
+router.get('/', async (req, res, next) => {
+  try {
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE,
+      ...url.parse(req.url, true).query,
+    })
+
+    const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
+    const data = apiRes.body
+
+    res.status(200).json(data)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
