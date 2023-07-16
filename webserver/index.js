0021f34534341dc6b0f1d62332fc29d19b7c535d
Dichitha <92388117+Dichitha@users.noreply.github.com>
2023-07-16 14:34:23 +0530

    Add files via upload

---
+++ webserver/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+require('dotenv').config();
+var url = require('url');
+const app = express();
+var server_port;
+var http = require('http');
+app.set('trust proxy', 1);
+
+var options = {
+  origin: 'http://localhost:9000',
+  optionsSuccessStatus: 200
+};
+
+
+app.use((req, res, next) => {
+  if (options.origin === req.headers.origin) {
+    next();
+  } else {
+    res.status(403).json({ error: 'Forbidden' });
+  }
+});
+
+// app.use((req, res, next) => {
+//   server_port = req.headers.destination;
+//   console.log(req.headers.destination);
+//   next();
+// });
+
+app.use('/api', require('./apiroute'));
+
+const ports = [5001, 5002, 5003];
+
+ports.forEach(function (port) { 
+  app.listen(port, () => console.log(`Server running on port ${port}`));
+});
