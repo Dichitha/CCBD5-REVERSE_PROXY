26a1242ad00fa60c0adbd5e0bfd314812d68e5bf
Dichitha <92388117+Dichitha@users.noreply.github.com>
2023-08-10 21:20:51 +0530

    Update requests.py

--- requests.py
+++ requests.py
@@ -1,10 +1,10 @@
 import requests
 from concurrent.futures import ThreadPoolExecutor
+import random
 
+url = "http://localhost:9009/api?q="
 
-url = "http://localhost:8008/api?q=Boston"
-
-num_requests = 999
+num_requests = 150
 hit_count = 0 
 miss_count = 0
 
@@ -12,6 +12,7 @@ def send_request(url):
     global hit_count
     global miss_count
     try:
+        url+= str(random.randint(0,20))
         response = requests.get(url)
         print(f"Request to {url} - Status code: {response.status_code}")
         hit_count += 1
@@ -20,7 +21,7 @@ def send_request(url):
         print(f"Request to {url} - Error: {str(e)}")
         miss_count += 1
 # Create a ThreadPoolExecutor to send multiple requests concurrently
-with ThreadPoolExecutor(max_workers=10) as executor:
+with ThreadPoolExecutor(max_workers=150) as executor:
     executor.map(send_request, [url] * num_requests)
 
 # Print the final hit count
